99dc111a17a67923f53b34b1694fd9cd33018169
Joje1283 <joje1283@gmail.com>
2024-03-04 07:12:51 +0900

    요청 처리 시간을 확인 하기 위한 미들 웨어 추가

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import Depends, FastAPI
+import time
+from fastapi import Depends, FastAPI, Request
 from secure import get_user, dget_user
 
 
@@ -19,3 +20,16 @@ async def dget_user(token: str = Depends(dget_user)):
     token decode by direct(jose)
     """
     return {"token": token}
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    """
+    add process time header
+    ref: https://fastapi.tiangolo.com/tutorial/middleware/
+    """
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
